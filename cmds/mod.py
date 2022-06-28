028ccb7695a960a1ddc6d97af8a0e6291001713c
Your Name <you@example.com>
2022-06-28 22:20:50 +0530

    ai stuff

--- cmds/mod.py
+++ cmds/mod.py
@@ -3,6 +3,8 @@ import discord
 
 from discord.ext import commands
 
+warns = {}
+
 class Mod(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
@@ -11,6 +13,25 @@ class Mod(commands.Cog):
     @commands.has_permissions(moderate_members=True)
     async def warn(self, ctx: commands.Context, member: discord.Member, *, reason: str = None):
         await ctx.send(f"{member.mention}, you have been warned for {reason}")
+        if member.id not in warns:
+            warns[member.id] = []
+        warns[member.id].append({"reason": reason, "by": ctx.author.id, "time": datetime.datetime.now()})
+        await ctx.say(f"Warning ID: {len(warns[member.id]) - 1}")
+
+    @commands.command()
+    @commands.has_permissions(moderate_members=True)
+    async def delwarn(self, ctx: commands.Context, member: discord.Member, index: int, *, reason: str = None):
+        await ctx.send(f"{member.mention}, you have had a warning removed for {reason}")
+        if member.id not in warns:
+            warns[member.id] = []
+        del warns[member.id][index]
+
+    @commands.command()
+    @commands.has_permissions(kick_members=True)
+    async def listwarns(self, ctx: commands.Context, member: discord.Member, *, reason: str = None):
+        if member.id not in warns:
+            warns[member.id] = []
+        await ctx.send(f"{member.mention}:\n\n{str(warns[member.id])}")
 
     @commands.command()
     async def kick(self, ctx: commands.Context, member: discord.Member, *, reason: str = None):
